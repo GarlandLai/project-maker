1e26f3d82ad2aac2a6ec04d02f4ae3c881b3321b
Garland Lai <garlandlai24@gmail.com>
2019-09-25 10:18:36 -0700

    following lessons on tutorial #10

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import { Switch, Route, BrowserRouter } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Dashboard from "./components/dashboard/Dashboard";
 import ProjectDetails from "./components/projects/ProjectDetails";
+import CreateProject from "./components/projects/CreateProject";
+import SignIn from './components/auth/SignIn';
+import SignUp from './components/auth/SignUp';
 
 class App extends Component {
   render() {
@@ -11,8 +14,11 @@ class App extends Component {
         <div className="App">
           <Navbar />
           <Switch>
-            <Route exact path="/" component={Dashboard}></Route>
-            <Route path="/project/:id" component={ProjectDetails}></Route>
+            <Route exact path="/" component={Dashboard} />
+            <Route path="/project/:id" component={ProjectDetails} />
+            <Route path="/create" component={CreateProject} />
+            <Route path="/signin" component={SignIn} />
+            <Route path="/signup" component={SignUp} />
           </Switch>
         </div>
       </BrowserRouter>
